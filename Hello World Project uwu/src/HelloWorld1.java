1cce2f6f08d45d510857930e50a0357a9f8043c8
APCS2nd <APCS2nd@MHS-LT9-20>
2019-08-21 12:16:56 -0700

    complete Hello World project

---
+++ Hello World Project uwu/src/HelloWorld1.java
@@ -0,0 +1,16 @@
+//Vivian Wong
+//August 19, 2019
+//First class iconic intro to Java for APCS 
+/*
+ * Prints a message to the screen
+ */
+
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+	}
+
+}
