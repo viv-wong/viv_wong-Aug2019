7df263e7c3fb046907634a547f38a00aff42371b
APCS2nd <APCS2nd@MHS-LT9-20>
2019-09-04 12:08:48 -0700

    Calculate Library part 1 unfinished

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,12 @@
+/* Vivian Wong
+ * August 29, 2019
+ * Period 4
+ * Group of methods that perform math calculations (a library)
+ */
+ 
+public class Calculate {
+	public static int square(int number) {
+		return number*number; 
+	}
+
+}
