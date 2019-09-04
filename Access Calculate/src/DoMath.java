fa78cd8a102907aa5868b9275f2d0ea6c2070c0e
APCS2nd <APCS2nd@MHS-LT9-20>
2019-09-04 12:02:43 -0700

    Access Calculate Part 1

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,14 @@
+/* Vivian Wong
+ * August 29, 2019
+ * Period 4
+ * To call math methods from our Calculate Library 
+ * Client code or runner code
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
