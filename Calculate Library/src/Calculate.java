2256a3b93c97db44bdb6ee0454a6a6797c5cbc43
APCS2nd <APCS2nd@MHS-LT9-20>
2019-09-11 12:53:02 -0700

    New Update for Part 1

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -6,37 +6,62 @@
 
  //Part 1
 
-public class Calculate {
+//returns the square of the value passed
+public class Calculate { 
 	public static int square(int number) {
-		return number*number;	
-		}
-	public static int cube(int number) {
-		return number*number*number;
-		}
-	public static double average(double x, double y){
-		return (x+y)/2;
-		}
-	public static double average(double x, double y, double z){
-		return (x+y+z)/3;
-		}
+		int answer=0;
+		answer=number*number;  
+		return answer;	
+	}
+	//returns the cube of the value passed
+	public static int cube(int number) { 
+		int answer=number*number*number;
+		return answer;
+	}
+	//returns the average of the values passed to it
+	public static double average(double x, double y){ 
+		double answer=(x+y)/2;
+		return answer;
+	}
+	// returns the average of 3 values passed to it;example of 'overloading'
+	public static double average(double x, double y, double z){ 
+		double answer=(x+y+z)/3;
+		return answer;
+	}
+	// converts an angle measure given in radians into degrees
 	public static double toDegrees(double number){
-		return number*(180/3.14159);
+		double answer=number*(180/3.14159);
+		return answer;
 	}
+	//converts an angle measure given in degrees into radians
 	public static double toRadians(double number) {
-		return number*(3.14159/180);
+		double answer= number*(3.14159/180);
+		return answer;
 	}
+	//returns the value of the discriminant
 	public static double discriminant(double a, double b, double c) {
-		return b*b-(4*a*c);
+		double answer=b*b-(4*a*c);
+		return answer;
 	}
-	public static String toImproperFrac(int a, int b, int c){
-		return"((a*c)+b)+/+ c";
+	//converts mixed number into an improper fraction
+	public static String toImproperFrac(int whole, int numerator, int denominator){
+		int answer= (whole*denominator)+numerator;
+		return answer + "/" +denominator;
 	}
-	public static String toFixedNum(int a, int b) { //check 31-38
-		return (a/b)+"/";
+	//converts an improper fraction into a mixed number
+	public static String toMixedNum(int a, int b) { 
+		;
+		return answer;
 	}
+	//converts a binomial multiplication form into a quadratic equation form
 	public static String foil(int a, int b, int c, int d, String n) {
-		return "(a*c)n +";
+		String answer= "(a*c)n +";
+		return answer;
 	}
+//Part Two
+	//determines whether or not one integer is evenly divisible by another
+	public static  isDivisibleBy(int x, int y)
+		
 	
 }
 
