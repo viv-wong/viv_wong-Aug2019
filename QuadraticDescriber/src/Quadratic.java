8d836d874b4ce76e19263257bbd64ae45d8da51c
Violetmaysilee <Violetmaysilee@10.5.4.209>
2019-09-30 18:52:56 -0700

    Calculate part 5 uWu

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -1,10 +1,84 @@
+//Vivian Wong
+//September 30, 2019
+//Actual code for Quadratic Describer 
+//responsible for all calculations
 
 public class Quadratic {
 	public static String quadrDescriber(double a, double b, double c) {
-		//quadDescriber, sqrt, discriminant, quad.form, round2
-	String output= "";
-		System.out.println("Description of the graph of:");
-			output= a+"x^2"+" + "+b+"x"+" + "+c;
-		
+		//use quadDescriber, sqrt, discriminant, quad.form, round2
+	//String output= "";
+	String description="";
+		//describing the quadratic form of the graph
+				description= "\nDescription of the graph of: \ny= " + (a+"x^2"+" + "+b+"x"+" + "+c);
+				
+		//finding the orientation of the graph; up or down	
+			if (a<0) {
+				description += "\nOpens: Down"; //print out string
+			} else {
+				description += "\nOpens: Up";
+		}
+		//finding the vertex
+			double h=(-b)/2*a; //first find h value
+			 if(b<0||b==0) {
+				 h=(+b)/2*a; //verify that a neg and neg equals a positive, as does a zero
+			 }
+			double k= a*(h*h)+b*h+c; //subs to find k value
+				description+= "\nVertex: "+"("+h+","+k+")"; //print out value
+				
+		//finding the axis of symmetry -- vertex's h
+				description+= "\nAxis of Symmetry: " + h;	 //print out h
+	return description;
+	}
+	
+	//finding the intercepts of x
+	public static String xIntercepts(double a, double b, double c) {
+		String output="";					//declare variables
+		double dis= discriminant(a,b,c);  //find discriminant
+			if (dis<0) {					//negative roots if dis<0 so no real roots
+				output= "x-intercept(s): no x-intercepts :(";
+				
+			} else if(dis==0) {				//if dis=0 they have the same root 
+				double root1=(-b+sqrt(dis))/(2*a); //find the root 		
+				double rootFinal1=round2(root1);	//round the root to 2 decimal places
+					output= "x-intercept(s): "+"("+ rootFinal1 + ", "+"0.0"+")";
+					
+			} else if(dis>0) { 				//if dis>0 there are 2 real roots
+				 double root3=(-b+sqrt(dis))/(2*a); //subtract and add the discriminant to find the roots
+				 double root2=(-b-sqrt(dis))/(2*a);
+					output= "x-intercept(s): "+ "("+ root2 + ", "+"0.0"+")"+ " & "+ "("+ root3 + ", "+"0"+")";
 			}
+			
+		return output;
+	}
+	
+	//finding y intercept -- just c
+		public static String yIntercept(double a, double b, double c) {
+			String output="";
+			output= "y-intercept: "+ c;
+			return output;
+		}
+			
+				
+	
+//small calculate library for above/necessary calculations in the 2nd method specifically
+public static double discriminant(double a, double b, double c) {
+	double answer1=(b*b); 	//'square' b and then subtract 4ac from the product
+	double answer2=(4*a*c);
+	return answer1-answer2;
 }
+public static double sqrt(double x) {
+	double guess=0.15*x;	//use guess and check to find an educated guess 
+	double answer=0.5*(x/guess + guess); //input into Newton's method to get an answer
+		double last=round2(answer);		//round the answer to 2 decimal places
+			return last;
+	}
+public static double round2(double x) {
+	double num1=(100*x)+0.5;	//multiply value by 100 to possible decimal points in the value back
+	num1=(int)num1; 			//add 0.5 to the product; next cast an int to num1 to make it a whole#
+	double answer=num1/100;		//divide by 100 to get decimal point in the correctly rounded 2 places
+	return answer;				//return answer
+	}
+}
+			
+	
+
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,36 +1,46 @@
 //Vivian Wong
-//September 27
-//Client code describe 
+//September 30, 2019
+//Client code for Quadratic Describer -- shows up on the console and handles interactions with the user
+//print out results from Quad Describer
 
 import java.util.*;
 
 public class QuadraticClient {
 	public static void main(String[]args) {
-		double a;
+		Scanner userInput = new Scanner(System.in); //set up a scanner
+		double a;		//initialize values
 		double b;
 		double c;
 		boolean contin = true;
-		Scanner userInput = new Scanner(System.in);
-		System.out.println("Please provide values for the coefficients a, b, and c");
-		System.out.print("a: \nb: \nc:");
+		System.out.println("Welcome to the Quadratic Describer! uWu"); //welcome message for user
+		while(contin==true) { //start loop
+			System.out.println("Please provide values for the coefficients a, b, and c"); //ask for input #s
+			System.out.print("a:");
 			a= userInput.nextDouble();
+			System.out.print("b:");
 			b= userInput.nextDouble();
+			System.out.print("c:");
 			c= userInput.nextDouble();
-		String description = Quadratic.quadrDescriber(a, b, c);
-		System.out.println(description);
-		System.out.println("Do you wish to continue? If not type \"quit\" to end");
-			answer= userInput.next();
-			if(contin==true)
-		
+			
+			String description = Quadratic.quadrDescriber(a, b, c); //call first line of description (orientation to axis of symm)
+			System.out.println(description);						//print out string
+			
+			String description2 = Quadratic.xIntercepts(a,b,c); //call 2nd line (x-intercepts)
+			System.out.println(description2);					//print out string
+			
+			String description3 = Quadratic.yIntercept(a, b, c); //call 3rd line (y-int)
+			System.out.println(description3);					//print out string
+			
+			System.out.print("\nDo you wish to continue? If not type \"quit\" to end\n"); //ask go again?
+			String answer= userInput.next(); 
+			if(answer.equalsIgnoreCase("quit")||answer.equalsIgnoreCase("no")) { //get a response
+				contin=false;
+			}
+		}
+		System.out.println("\nThank you for your service!"); //thank you message for user
+		System.out.print("Have a nice day! :))");
 	}
 }
 
-/*set up a scanner
-welcome message
-start game loop
-ask for input
-call quadrDescriber and send it the inputs
-print out returned string
-ask go again
-get a response
-*/
+
+
